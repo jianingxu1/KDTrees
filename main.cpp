f3e0edb2d354f3b4134b6f2f10d51a6f786753c6
Jianing Xu <58917306+jianingxu1@users.noreply.github.com>
2023-10-04 00:04:51 +0200

    Update sample tests in main

--- main.cpp
+++ main.cpp
@@ -2,16 +2,23 @@
 #include <vector>
 
 #include "KDTree.h"
+#include "Point.h"
 using namespace std;
 
 int main() {
   KDTree tree;
-  tree.insert(Point({10, 20}));
-  tree.insert(Point({5, 30}));
-  tree.insert(Point({8, 40}));
-  tree.insert(Point({15, 25}));
-  tree.insert(Point({100, 30}));
-  tree.insert(Point({7, 15}));
+  tree.insert(Point({0.3, 0.8}));
+  tree.insert(Point({0.2, 0.4}));
+  tree.insert(Point({0.5, 0.6}));
+  tree.insert(Point({0.6, 0.1}));
+  tree.insert(Point({0.8, 0.4}));
+  tree.insert(Point({1.0, 0.2}));
+  tree.insert(Point({0.9, 0.8}));
+  tree.insert(Point({0.7, 0.6}));
+  tree.insert(Point({0.75, 0.65}));
   tree.print();
-  return 0;
+
+  Point p = tree.findNearestNeighbor(Point({0.7, 0.7}));
+  p.print();
+  cout << endl;
 }

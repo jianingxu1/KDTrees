eaa646eafc49c93358361645697f9c471cf4d493
Jianing Xu <58917306+jianingxu1@users.noreply.github.com>
2023-10-05 18:40:16 +0200

    Implement automatization of KDTrees experimentation

--- main.cpp
+++ main.cpp
@@ -1,24 +1,50 @@
+#include <time.h>
+
 #include <iostream>
+#include <random>
 #include <vector>
 
 #include "KDTree.h"
-#include "Point.h"
 using namespace std;
 
-int main() {
-  KDTree tree;
-  tree.insert(Point({0.3, 0.8}));
-  tree.insert(Point({0.2, 0.4}));
-  tree.insert(Point({0.5, 0.6}));
-  tree.insert(Point({0.6, 0.1}));
-  tree.insert(Point({0.8, 0.4}));
-  tree.insert(Point({1.0, 0.2}));
-  tree.insert(Point({0.9, 0.8}));
-  tree.insert(Point({0.7, 0.6}));
-  tree.insert(Point({0.75, 0.65}));
-  tree.print();
+// Random uniform number generator between 0 and 1
+mt19937 gen(time(NULL));
+uniform_real_distribution<float> dis(0.0f, 1.0f);
 
-  Point p = tree.findNearestNeighbor(Point({0.7, 0.7}));
-  p.print();
-  cout << endl;
+vector<Point> generateQueries(int numQueries, int k) {
+  vector<Point> queries(numQueries);
+  for (int i = 0; i < numQueries; ++i) {
+    vector<float> coords(k);
+    for (int j = 0; j < k; ++j) {
+      coords[j] = dis(gen);
+    }
+    Point p(coords);
+    queries[i] = p;
+  }
+  return queries;
+}
+
+int main() {
+  int numTrees, numNodes, numQueries, k;
+  cout << "Introduce the number of trees, nodes, queries and dimensions:\n";
+  cin >> numTrees >> numNodes >> numQueries >> k;
+
+  vector<Point> queries = generateQueries(numQueries, k);
+
+  // Create trees one by one, and execute queries
+  for (int i = 0; i < numTrees; ++i) {
+    // Create tree
+    KDTree tree(numNodes, k);
+
+    // Execute queries
+    int nodesVisited = 0;
+    for (int j = 0; j < numQueries; ++j) {
+      int curNodesVisited = 0;
+      tree.findNearestNeighbor(queries[j], curNodesVisited);
+      nodesVisited += curNodesVisited;
+      // cout << curNodesVisited << endl;
+    }
+    int avgNodesVisited = nodesVisited /= numQueries;
+    cout << avgNodesVisited << endl;
+  }
 }

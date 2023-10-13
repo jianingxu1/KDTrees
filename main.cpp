908b8a259ebe1d86aab4c4aa02abab4fe02e34d1
Yasin <146195255+yasin281@users.noreply.github.com>
2023-10-14 00:12:27 +0200

    Update numTrees, from 10 to 200

--- main.cpp
+++ main.cpp
@@ -45,7 +45,7 @@ int main() {
 
   outputFile << "Dimension: "<< k << endl;
 
-  numTrees = 10;
+  numTrees = 100;
   numNodes = 1000;
   numQueries = 10000;  
 
@@ -53,7 +53,7 @@ int main() {
     
     outputFile << typeTree[typeOfTree] << endl;
 
-    for (int n = numNodes; n <= 1000000; n+=2000) { //For que itera por diferentes tamaños de arboles
+    for (int n = numNodes; n <= 500000; n+=2000) { //For que itera por diferentes tamaños de arboles
 
       outputFile << "Number of Nodes: "<<n << " Means: ";
 
